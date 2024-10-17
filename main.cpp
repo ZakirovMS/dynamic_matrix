26df15dd7877f9a85b15f90cb4354d037905af97
ZakirovMS <mz.08.08.06@gmail.com>
2024-10-17 11:18:45 +0300

    Creating function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
+int ** create (size_t m, size_t n);
+void clean(int ** mtx, size_t m, size_t n);
+
 int main()
 {
   size_t M = 0, N = 0;
   std::cin >> M >> N;
+  int ** mtx = create(M, N);
+  clean (mtx, M, N);
 }
-
