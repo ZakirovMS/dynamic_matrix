5e74ed82e5d808f9e966b4759864dce2ad3b7cfe
ZakirovMS <mz.08.08.06@gmail.com>
2024-11-04 23:43:32 +0300

    OOP class implementation

--- mtx.cpp
+++ mtx.cpp
@@ -67,3 +67,137 @@ void read(const int * const * mtx, size_t m, size_t n)
     }
   }
 }
+
+Mtx::Mtx(size_t m, size_t n)
+{
+  int ** mtx = new int * [m];
+  size_t counter = 0;
+  try
+  {
+    for (; counter < m; ++counter)
+    {
+      mtx[counter] = new int[n];
+    }
+  }
+  catch(const std::bad_alloc &e)
+  {
+    clean(mtx, counter);
+    throw;
+  }
+
+  mtx_ = mtx;
+  m_ = m;
+  n_ = n;
+}
+
+Mtx::Mtx(int ** mtx, size_t m, size_t n)
+{
+  int ** new_mtx = new int * [m];
+  size_t counter = 0;
+  try
+  {
+    for (; counter < m; ++counter)
+    {
+      mtx[counter] = new int[n];
+    }
+  }
+  catch(const std::bad_alloc &e)
+  {
+    clean(mtx, counter);
+    throw;
+  }
+
+  for (size_t i = 0; i < m; ++i)
+  {
+    for (size_t j = 0; j < n; ++j)
+    {
+      new_mtx[m][n] = mtx[m][n];
+    }
+  }
+
+  mtx_ = new_mtx;
+}
+
+Mtx::~Mtx()
+{
+  for(size_t i = 0; i < m_; ++i)
+  {
+    delete[] mtx_[i];
+  }
+
+  delete[] mtx_;
+}
+
+void Mtx::read(const int * const * mtx, size_t m, size_t n)
+{
+  for (size_t i = 0; i < m; ++i)
+  {
+    for (size_t j = 0; j < n; ++ j)
+    {
+      std::cout << mtx[i][j];
+
+      if ((j+1) != n)
+      {
+        std::cout << " ";
+      }
+      else
+      {
+        std::cout << "\n";
+      }
+
+    }
+  }
+}
+
+void Mtx::write(int ** mtx, size_t m, size_t n)
+{
+  for (size_t i = 0; i < m; ++i)
+  {
+    for (size_t j = 0; j < n; ++j)
+    {
+      std::cin >> mtx[i][j];
+    }
+  }
+}
+
+size_t Mtx::get_columns() const
+{
+  return m_;
+}
+
+size_t Mtx::get_rows() const
+{
+  return n_;
+}
+
+void Mtx::change_size(size_t new_m, size_t new_n)
+{
+  size_t columns = new_m < m_ ? new_m : m_;
+  size_t rows = new_n < n_ ? new_n : n_;
+  int ** chsize_mtx = new int * [columns];
+  size_t counter = 0;
+  try
+  {
+    for (; counter < columns; ++counter)
+    {
+      chsize_mtx[counter] = new int[rows];
+    }
+  }
+  catch(const std::bad_alloc &e)
+  {
+    clean(chsize_mtx, counter);
+    throw;
+  }
+
+  for (size_t i = 0; i < columns; ++i)
+  {
+    for (size_t j = 0; j < rows; ++j)
+    {
+      chsize_mtx[columns][rows] = mtx_[columns][rows];
+    }
+  }
+
+  mtx_ = chsize_mtx;
+  m_ = new_m;
+  n_ = new_n;
+}
--- mtx.hpp
+++ mtx.hpp
@@ -5,4 +5,22 @@ int ** create(size_t m, size_t n);
 void clean(int ** mtx, size_t m);
 void read(const int * const * mtx, size_t m, size_t n);
 void write(int ** mtx, size_t m, size_t n);
+struct Mtx
+{
+  Mtx(size_t m, size_t n);
+  Mtx(int ** old_mtx, size_t m, size_t n);
+  ~Mtx();
+
+  void read(const int * const * mtx, size_t m, size_t n);
+  void write(int ** mtx, size_t m, size_t n);
+
+  size_t get_columns() const;
+  size_t get_rows() const;
+  void change_size(size_t new_m, size_t new_n);
+
+  private:
+    int **mtx_;
+    size_t m_;
+    size_t n_;
+};
 #endif
